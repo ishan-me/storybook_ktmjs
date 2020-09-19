e92b1b72a50b4011e4882e68794d892c0fcd0408
Ishan Manandhar <ishan02016@gmail.com>
2020-09-19 18:26:17 +0545

    added some base components

--- src/components/MaterialUI/MaterialButton.stories.js
+++ src/components/MaterialUI/MaterialButton.stories.js
@@ -6,16 +6,43 @@ import Button from '@material-ui/core/Button';
 export default {
   title: 'Material/Button',
   component: Button,
+  argTypes: {
+    onClick: { action: 'clicked button' },
+  },
 };
 
-export const Primary = () => (
-  <Button variant="contained" color="primary">
-    Primary
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: 'contained',
+  color: 'primary',
+  children: 'Template Primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  variant: 'contained',
+  color: 'secondary',
+  children: 'Template Secondary',
+};
+
+// example of console addon
+
+export const Logger = () => (
+  <Button
+    variant="contained"
+    color="primary"
+    onClick={() => console.log('Logger clicked')}
+  >
+    Logger Button
   </Button>
 );
 
-export const Secondary = () => (
-  <Button variant="contained" color="secondary">
-    Secondary
-  </Button>
+// Accessibility addon
+
+export const accessibleButton = () => <button>Accessible button</button>;
+
+export const inaccessibleButton = () => (
+  <Button style={{ backgroundColor: 'green', color: 'darkRed' }}>Inaccessible button</Button>
 );
