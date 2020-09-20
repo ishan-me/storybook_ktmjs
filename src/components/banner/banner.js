dda7e3debde867b489abef378d5e694c581c9877
Ishan Manandhar <ishan02016@gmail.com>
2020-09-20 14:18:20 +0545

    example

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Title from '../title/title';
 import Button from '../buttons/button';
 
-import './banner.css'
+import './banner.css';
 
 function HeroBanner({ variant = 'bannerWrapper', ...rest }) {
   return (
     <section className={`${variant}`} {...rest}>
-      <Title variant = 'Headingonewhite'>This is a big title of the banner</Title>
+      <Title variant="Headingonewhite">This is a big title of the banner</Title>
       <Button variant="secondary">A Button</Button>
     </section>
   );
